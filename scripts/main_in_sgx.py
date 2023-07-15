f5bd1d28385d80fef8eb2c50644660b7613c7c87
York <York.Yanmeng@gmail.com>
2023-07-16 00:52:42 +0800

    2023/07/16

--- scripts/main_in_sgx.py
+++ scripts/main_in_sgx.py
@@ -24,14 +24,22 @@ def _pack(key, m):
     return buf.read()
 
 
+def get_len(msg):
+    buf = BytesIO()
+    buf.write(struct.pack("<i", len(msg)))
+    buf.write(msg)
+    buf.seek(0)
+    return buf.read()
+
+
 def client_send(tx):
     # FROM SERVER TRUSTED PART TO SERVER UNTRUSTED PART
     host = 'localhost'
     port = 30000
     sk = socket.socket()
     sk.connect((host, port))
-
-    sk.sendall(tx)
+    _tx = get_len(tx)
+    sk.sendall(_tx)
     # data = sk.recv(1024)
     print("SEND OK")
     sk.close()
@@ -58,22 +66,28 @@ def start_server():
 
 
 def handle_client(client_socket):
+    data = b''
     while True:
-        data = client_socket.recv(1024)
+        data += client_socket.recv(1024)
         if not data:
             break
         else:
+            buf = BytesIO(data)
+            size, = struct.unpack("<i", buf.read(4))
+            tx = buf.read(size)
+            if len(data) - 4 != size:
+                continue
             try:
-                unpack_data = _unpack(data)
+                unpack_data = _unpack(tx)
                 if unpack_data == "":
                     print("DECRYPT FAILED")
                     break
                 else:
                     key = _hash(unpack_data)
-                    print("DECRYPT OK, DATA => ", data)
-                    tx = _pack(key, data)
-                    client_send(tx)
-
+                    print("DECRYPT OK, DATA => ", tx)
+                    _tx = _pack(key, tx)
+                    client_send(_tx)
+                break
             except Exception as e:
                 print("EXCEPTION => ", e)
                 break
